07d2eb0ee1393ab02522785097eedb19a0a30ebd
Fitsum A <fitsum@gmail.com>
2019-11-26 10:47:10 -0500

    Addin Blog Model ..

--- Blog/models.py
+++ Blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=50)
+    text = models.TextField(default = "This is Default Text")
+    pubdate = models.DateTimeField(default = timezone.now())
+
+    def __str__(self):
+        return self.title
+    
