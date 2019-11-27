b0a06d1d6ce95678f2706927df10fd00a40940b8
Fitsum A <fitsum@gmail.com>
2019-11-27 09:12:43 -0500

    Adding Values and Managin on admin dashboard  ..

--- Blog/models.py
+++ Blog/models.py
@@ -4,8 +4,8 @@ from django.utils import timezone
 # Create your models here.
 class Blog(models.Model):
     title = models.CharField(max_length=50)
-    text = models.TextField(default = "This is Default Text")
-    pubdate = models.DateTimeField(default = timezone.now())
+    text = models.TextField()
+    pubdate = models.DateTimeField()
 
     def __str__(self):
         return self.title
